63b3717fe49e1c831a291ca5aceb474c63e1878c
01ahsan <105579750+01ahsan@users.noreply.github.com>
2026-09-15 20:12:24 +0600

    Align TestPyPI workflow identity

--- src/temprior/__init__.py
+++ src/temprior/__init__.py
@@ -22,7 +22,7 @@ from .uncertainty import (
 )
 from .data import default_prior, make_calibrated_prior, make_example_outbreak
 
-__version__ = "0.1.0rc2"
+__version__ = "0.1.0rc3"
 
 __all__ = [
     "FeatureSpec",
